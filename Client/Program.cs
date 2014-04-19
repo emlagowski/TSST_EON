86ff58258d24b1a6134cce76e977fcb5ac199d11
pawello234 <pawello234@o2.pl>
2014-04-19 21:17:11 +0200

    nie wiem

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,6 +10,15 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Client cl1 = new Client();
+            Client cl2 = new Client();
+            cl1.connect(1);
+            cl2.connect(1);
+            cl1.send("Pierwszy do drugiego");
+            Console.ReadLine();
+
+
+
         }
     }
 }
