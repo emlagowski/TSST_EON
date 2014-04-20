9b9daf5de36c142e16143187e05952e4c264d248
pawello234 <pawello234@o2.pl>
2014-04-20 12:56:04 +0200

    client

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -10,7 +10,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Put receiving port (1 or 3).");
+           /* Console.WriteLine("Put receiving port (1 or 3).");
             Int32 a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Put sending port (2 or 4).");
             Int32 b = Convert.ToInt32(Console.ReadLine());
@@ -18,12 +18,12 @@ namespace Client
             cl.connect(new EndPoint(b, a));
             Console.WriteLine("Push 'Enter' to connect.");
             Console.ReadLine();
-            cl.send("tesssstab");
-            //Client cl1 = new Client();
-            //Client cl2 = new Client();
-            //cl1.connect(new EndPoint(2,1));
-            //cl2.connect(new EndPoint(4,3));
-            //cl1.send("Pierwszy do drugiego");
+            cl.send("tesssstab");*/
+            Client cl1 = new Client();
+            Client cl2 = new Client();
+            cl1.connect(new EndPoint(2,1));
+            cl2.connect(new EndPoint(4,3));
+            cl1.send("Pierwszy do drugiego");
             Console.ReadLine();
 
             //bbb
