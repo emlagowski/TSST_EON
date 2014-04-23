25630d98fd25dcb8eb88a533f0edb33e35036fac
mlody991 <mlody991@gmail.com>
2014-04-23 21:45:05 +0200

    newnewnew

---
+++ ClientNew/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ClientNew
+{
+    class Client
+    {
+        int _port;
+        string _address;
+        TcpClient _client;
+        Boolean _isOn;
+
+        public Client(String address, int port)
+        {
+            _port = port;
+            _address = address;
+            TcpClient client = new TcpClient();
+            _isOn = false;
+        }
+
+        public void connect()
+        {
+            try
+            {
+                _client.Connect(_address, _port);
+                _isOn = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
