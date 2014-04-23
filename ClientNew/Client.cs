26f3cdd06008b0f17e8cc98e9b4c089637cec6ea
mlody991 <mlody991@gmail.com>
2014-04-24 00:53:48 +0200

    brand new client and cloud

--- ClientNew/Client.cs
+++ ClientNew/Client.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace ClientNew
 {
@@ -11,6 +13,7 @@ namespace ClientNew
     {
         int _port;
         string _address;
+        TcpListener _listener;
         TcpClient _client;
         Boolean _isOn;
 
@@ -18,8 +21,21 @@ namespace ClientNew
         {
             _port = port;
             _address = address;
-            TcpClient client = new TcpClient();
+            _listener = new TcpListener(IPAddress.Parse(_address), _port);
+            _listener.Start();
             _isOn = false;
+            Thread t = new Thread(Run);
+            t.Start();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("waiting ...");
+                _client = _listener.AcceptTcpClient();
+                Console.WriteLine("got something");
+            }
         }
 
         public void connect()
--- ClientNew/Program.cs
+++ ClientNew/Program.cs
@@ -10,6 +10,14 @@ namespace ClientNew
     {
         static void Main(string[] args)
         {
+            Client c1 = new Client("127.0.0.1", 3);
+            Client c2 = new Client("127.0.0.2", 5);
+            Client c3 = new Client("127.0.0.3", 7);
+            Client c4 = new Client("127.0.0.4", 9);
+            Client c5 = new Client("127.0.0.5", 15);
+            Client c6 = new Client("127.0.0.6", 20);
+
+            Console.ReadLine();
         }
     }
 }
