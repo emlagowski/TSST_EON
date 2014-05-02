acd7156d9b856ffa914037c216ee20691375658a
pawello234 <pawello234@o2.pl>
2014-05-02 23:20:20 +0200

    wireband class

---
+++ FinalClient/WireBand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalClient
+{
+    class WireBand
+    {
+        int wireID;
+        int lambdaCapactity;
+        Boolean[] lambdas;
+        public WireBand(int id, int capacity)
+        {
+            wireID = id;
+            lambdaCapactity = capacity;
+            lambdas = new Boolean[capacity];
+            for (int i = 0; i < lambdas.Length; i++)
+            {
+                lambdas[i] = true;
+            }
+        }
+    }
+}
