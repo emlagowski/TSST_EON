dc73f54f605575fa96f025dabed015da89d4b5b0
pawello234 <pawello234@o2.pl>
2014-05-03 16:12:19 +0200

    naprawilem swoje zmiany prosze kurwa uwazac przy mergowaniu

--- FinalClient/Signaling.cs
+++ FinalClient/Signaling.cs
@@ -7,63 +7,68 @@ using System.Net.Sockets;
 using System.Net;
 using System.Threading;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
 
 namespace FinalClient
 {
     class Signaling
     {
-       IPEndPoint signalingEP, cloudSignalingEP;
-       Socket signalingSocket;
-       private ManualResetEvent signalingReceive = new ManualResetEvent(false);
-       private ManualResetEvent connectDone = new ManualResetEvent(false);
-       private ManualResetEvent receiveDone = new ManualResetEvent(false);
-       private String response = String.Empty;
+        IPEndPoint signalingEP, cloudSignalingEP;
+        Socket signalingSocket;
+        private ManualResetEvent signalingReceive = new ManualResetEvent(false);
+        private ManualResetEvent connectDone = new ManualResetEvent(false);
+        private ManualResetEvent receiveDone = new ManualResetEvent(false);
+        private ManualResetEvent sendDone = new ManualResetEvent(false);
+
+        private String response = String.Empty;
 
 
-       private List<WireBand> AvalaibleBandIN;
-       private List<WireBand> AvalaibleBandOUT;
-       private List<Connection> Connections;
+        private List<WireBand> AvalaibleBandIN;
+        private List<WireBand> AvalaibleBandOUT;
+        private List<Connection> Connections;
 
-       public Signaling()
-       {
-           initWireBand();//signaling
-           //signaling
-           signalingEP = new IPEndPoint(IPAddress.Parse("127.6.6.6"), 6666);
-           cloudSignalingEP = new IPEndPoint(IPAddress.Parse("127.6.6.5"), 6666);
-           signalingSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-           signalingSocket.Bind(signalingEP);
-           signalingSocket.BeginConnect(cloudSignalingEP,
-                   new AsyncCallback(ConnectCallback), signalingSocket);
-           connectDone.WaitOne();
+        public Signaling()
+        {
+            initWireBand();
+            //NARAZIE ZAKOMENTOWANE BO TO DO 2 ETAPU
 
-           Thread t0 = new Thread(RunSignaling);
-           t0.Start();
-       }
+            /*   signalingEP = new IPEndPoint(IPAddress.Parse("127.6.6.6"), 6666);
+               cloudSignalingEP = new IPEndPoint(IPAddress.Parse("127.6.6.5"), 6666);
+               signalingSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+               signalingSocket.Bind(signalingEP);
+               signalingSocket.BeginConnect(cloudSignalingEP,
+                       new AsyncCallback(ConnectCallback), signalingSocket);
+               connectDone.WaitOne();
 
-       private void RunSignaling()
-       {
-           try
-           {
-               while (true)
-               {
-                   signalingReceive.Reset();
-                   Console.WriteLine("Waiting for signaling data...");
-                   Receive();
-                   signalingReceive.WaitOne();
-               }
+               Thread t0 = new Thread(RunSignaling);
+               t0.Start();*/
+        }
 
-           }
-           catch (Exception e)
-           {
-               Console.WriteLine(e.ToString());
-           }
-       }
+        private void RunSignaling()
+        {
+            try
+            {
+                while (true)
+                {
+                    signalingReceive.Reset();
+                    Console.WriteLine("SIGNALING: Waiting for signaling data...");
+                    Receive();
+                    signalingReceive.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
         public void Receive()
         {
             try
             {
                 // Create the state object.
-                StateObject state = new StateObject();
+                SignalingStateObject state = new SignalingStateObject();
                 state.workSocket = signalingSocket;
                 //response = String.Empty;
                 // Begin receiving the data from the remote device.
@@ -81,31 +86,47 @@ namespace FinalClient
             {
                 // Retrieve the state object and the client socket 
                 // from the asynchronous state object.
-                StateObject state = (StateObject)ar.AsyncState;
+                SignalingStateObject state = (SignalingStateObject)ar.AsyncState;
                 Socket client = state.workSocket;
+
+                // byte[] clientData = new byte[1024 * 1024 * 50];
+
+                // int receivedBytesLen = clientSock.Receive(clientData);
 
                 // Read data from the remote device.
                 int bytesRead = client.EndReceive(ar);
 
-                if (bytesRead > 0)
-                {
-                    // There might be more data, so store the data received so far.
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                BinaryFormatter formattor = new BinaryFormatter();
 
-                    // Get the rest of the data.
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                        new AsyncCallback(ReceiveCallback), state);
-                }
-                else
-                {
-                    // All the data has arrived; put it in response.
-                    if (state.sb.Length > 1)
-                    {
-                        response = state.sb.ToString();
-                    }
-                    // Signal that all bytes have been received.
-                    receiveDone.Set();
-                }
+                MemoryStream ms = new MemoryStream(state.buffer);
+
+                state.conn = (Connection)formattor.Deserialize(ms);
+
+                Connections.Add(state.conn);
+                //TUTAJ JESCZE TRZEBA BEDZIE DOROBIC ZE JAK OTRZYMA OBIEKT TO MUSI ZAALOKOWAC OBPOWIEDNIE PASMA W POLACZENIACH IN I OUT.
+
+
+                /* if (bytesRead > 0)
+                 {
+                     // There might be more data, so store the data received so far.
+                     //state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                     System.arraycopy(state.buffer, 0, state.result, 0, state.buffer.length);
+
+                     // Get the rest of the data.
+                     client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                         new AsyncCallback(ReceiveCallback), state);
+                 }
+                 else
+                 {
+                     // All the data has arrived; put it in response.
+                     if (state.sb.Length > 1)
+                     {
+                         response = state.sb.ToString();
+                     }
+                     // Signal that all bytes have been received.
+                     receiveDone.Set();
+                 }*/
+                receiveDone.Set();
             }
             catch (Exception e)
             {
@@ -113,41 +134,111 @@ namespace FinalClient
             }
         }
 
-       private void ConnectCallback(IAsyncResult ar)
-       {
-           try
-           {
-               // Retrieve the socket from the state object.
-               Socket client = (Socket)ar.AsyncState;
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket client = (Socket)ar.AsyncState;
 
-               // Complete the connection.
-               client.EndConnect(ar);
+                // Complete the connection.
+                client.EndConnect(ar);
 
-               Console.WriteLine("{0} Socket connected to {1}", client.LocalEndPoint.ToString(), client.RemoteEndPoint.ToString());
+                Console.WriteLine("SIGNALING: {0} Socket connected to {1}", client.LocalEndPoint.ToString(), client.RemoteEndPoint.ToString());
 
-               // Signal that the connection has been made.
-               connectDone.Set();
-           }
-           catch (Exception e)
-           {
-               Console.WriteLine(e.ToString());
-           }
-       }
+                // Signal that the connection has been made.
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void Send(Socket client, Connection conn)
+        {
+            MemoryStream fs = new MemoryStream();
 
-       private void initWireBand()
-       {
-           //trzeba dorobic potem
-           //TEST
-           AvalaibleBandIN = new List<WireBand>();
-           AvalaibleBandOUT = new List<WireBand>();
+            BinaryFormatter formatter = new BinaryFormatter();
 
-           AvalaibleBandIN.Add(new WireBand(1, 12));
-           AvalaibleBandIN.Add(new WireBand(2, 10));
-           AvalaibleBandOUT.Add(new WireBand(1, 6));
-           AvalaibleBandOUT.Add(new WireBand(2, 8));
+            formatter.Serialize(fs, conn);
+
+            byte[] buffer = fs.ToArray();
 
 
-       }
+
+            // Begin sending the data to the remote device.
+            client.BeginSend(buffer, 0, buffer.Length, 0,
+                new AsyncCallback(SendCallback), client);
+            sendDone.WaitOne();
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket client = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("SIGNALING: Sent {0} bytes to server.", bytesSent);
+
+                // Signal that all bytes have been sent.
+                sendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        private void initWireBand()
+        {
+
+            AvalaibleBandIN = new List<WireBand>();
+            AvalaibleBandOUT = new List<WireBand>();
+
+
+            String xmlString = File.ReadAllText("wires.xml");
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                while (reader.ReadToFollowing("wire"))
+                {
+                    reader.MoveToAttribute("Id");
+                    string id = reader.Value;
+                    reader.MoveToNextAttribute();
+                    string capacity = reader.Value;
+                    reader.MoveToNextAttribute();
+                    string distance = reader.Value;
+
+                    AvalaibleBandIN.Add(new WireBand(Convert.ToInt32(id), Convert.ToInt32(capacity), Convert.ToInt32(distance)));
+
+
+                }
+            }
+
+        }
+
+        public void checkIfConnEstablished()
+        {
+
+        }
+
+        private void updateLambdas()
+        {
+
+        }
     }
-
+    public class SignalingStateObject
+    {
+        // Client socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 256;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        public Connection conn { get; set; }
+    }
 }
+
