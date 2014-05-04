c79fa358fe0f6def11313a30bfe9deac09c24049
mlody991 <mlody991@gmail.com>
2014-05-04 15:47:42 +0200

    xmls up

---
+++ FinalClient/UnexpectedFIB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalClient
+{
+    class UnexpectedFIB
+    {
+        List<AddressPair> addressList;
+
+        public UnexpectedFIB()
+        {
+            addressList = new List<AddressPair>();
+        }
+
+        public void addNext(String one, String two)
+        {
+            addressList.Add(new AddressPair(one, two));
+        }
+
+
+        public String findTarget(String ip)
+        {
+            String result = "";
+            for (int i = 0; i < addressList.Count; i++)
+            {
+                if (addressList[i].addresOne == ip)
+                {
+                    result = addressList[i].addressTwo;
+                }
+            }
+            return result;
+        }
+
+        class AddressPair
+        {
+            public String addresOne, addressTwo;
+
+            public AddressPair(String one, String two)
+            {
+                addresOne = one;
+                addressTwo = two;
+            }
+        }
+    }
+}
