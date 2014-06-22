f329a89c389ea5feabbd7120e37aace5567a691f
mlody991 <mlody991@gmail.com>
2014-06-22 14:28:26 +0200

    dijkstra

---
+++ DijkstraAlgorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] L ={
+                {-1,  5, -1, -1, -1,  3, -1, -1}, 
+                { 5, -1,  2, -1, -1, -1,  3, -1}, 
+                {-1,  2, -1,  6, -1, -1, -1, 10}, 
+                {-1, -1,  6, -1,  3, -1, -1, -1},
+                {-1, -1, -1,  3, -1,  8, -1,  5}, 
+                { 3, -1, -1, -1,  8, -1,  7, -1}, 
+                {-1,  3, -1, -1, -1,  7, -1,  2}, 
+                {-1, -1, 10, -1,  5, -1,  2, -1} 
+            };
+
+            EvilDijkstra ed = new EvilDijkstra();
+            ed.test();
+            Console.ReadLine();
+
+        }
+    }
+}
