b8a55470ad5e0b146c0d953a9e153db505468b53
mlody991 <mlody991@gmail.com>
2014-12-16 02:09:08 +0100

    bats

---
+++ Client.bat
@@ -0,0 +1,2 @@
+set /p pathName=Enter Client ID:
+start User\bin\Debug\Client.exe 127.0.0.%pathName%
---
+++ Router.bat
@@ -0,0 +1,2 @@
+set /p pathName=Enter Router ID:
+start FinalClient\bin\Debug\Router.exe 127.0.1.%pathName% false
